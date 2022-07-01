3f8a999d6ef4ce40d63ed9e84c09e000d5f79485
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-07-01 23:53:06 +0530

    nothing done

---
+++ src/main/java/com/example/boom/MyBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package com.example.boom;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(beanName+" before");
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(beanName+" after");
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
--- src/main/java/com/example/boom/Sphere.java
+++ src/main/java/com/example/boom/Sphere.java
@@ -1,9 +1,26 @@
 package com.example.boom;
 
-public class Sphere implements Shape {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.List;
+
+public class Sphere implements Shape, ApplicationContextAware {
 
     String name;
+    private ApplicationContext applicationContext;
 
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+
+    List<String> names;
     // constructor injection
     Sphere(String name){
         this.name = name;
@@ -13,6 +30,7 @@ public class Sphere implements Shape {
     }
 
     // setter injection
+    @Required
     public void setRadius(int radius) {
         this.radius = radius;
     }
@@ -20,6 +38,11 @@ public class Sphere implements Shape {
     int radius;
     @Override
     public void draw() {
-        System.out.println("I am " + name + " goli moli with radius "+radius);
+        System.out.println("I am " + name + " goli moli with radius "+radius+" elo "+ names.get(0)+" "+names.get(1));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
