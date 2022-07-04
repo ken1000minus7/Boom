bb06352d6583a591a656049e68b60bbae84613d3
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-07-04 20:01:50 +0530

    no progress

--- src/main/java/com/example/boom/BoomApplication.java
+++ src/main/java/com/example/boom/BoomApplication.java
@@ -3,15 +3,21 @@ package com.example.boom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class BoomApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        context.registerShutdownHook();
         Shape shape = (Shape) context.getBean("sphere");
         shape.draw();
+        System.out.println(context.getMessage("hello_world",null,"dead",null));
+        MessageSource messageSource = (MessageSource) context.getBean("messageSource");
+        System.out.println(messageSource.getMessage("sadgers",null,"aha",null));
         SpringApplication.run(BoomApplication.class, args);
     }
 
--- src/main/java/com/example/boom/Sphere.java
+++ src/main/java/com/example/boom/Sphere.java
@@ -1,15 +1,25 @@
 package com.example.boom;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 import java.util.List;
 
 public class Sphere implements Shape, ApplicationContextAware {
 
     String name;
+    // field injection
+    @Autowired
+    private MessageSource messageSource;
     private ApplicationContext applicationContext;
 
     public List<String> getNames() {
@@ -30,7 +40,7 @@ public class Sphere implements Shape, ApplicationContextAware {
     }
 
     // setter injection
-    @Required
+    @Autowired
     public void setRadius(int radius) {
         this.radius = radius;
     }
@@ -38,11 +48,21 @@ public class Sphere implements Shape, ApplicationContextAware {
     int radius;
     @Override
     public void draw() {
-        System.out.println("I am " + name + " goli moli with radius "+radius+" elo "+ names.get(0)+" "+names.get(1));
+        System.out.println(messageSource.getMessage("sphere_message",new Object[]{radius,names.get(0),names.get(1)},"life",null));
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void createSphere(){
+        System.out.println("Sphere created");
+    }
+
+    @PreDestroy
+    public void destroySphere(){
+        System.out.println("Sphere destroyed");
     }
 }
